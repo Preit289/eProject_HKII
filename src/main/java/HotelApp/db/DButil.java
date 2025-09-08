9eb6b3ba6086128aa1535682087db2ddba6e8e09
Preit289 <hoanthinh.david@gmail.com>
2025-09-08 18:37:46 +0700

    Update DBUtil

--- src/main/java/HotelApp/db/DButil.java
+++ src/main/java/HotelApp/db/DButil.java
@@ -3,10 +3,9 @@ package HotelApp.db;
 import java.sql.*;
 
 public class DButil {
-    private static final String URL  =
-        "jdbc:sqlserver://localhost:1433;databaseName=HotelMng;encrypt=true;trustServerCertificate=true";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=HotelMng;trustServerCertificate=true;integratedSecurity=true;";
     private static final String USER = "sa";
-    private static final String PASS = "Preit289##$";
+    private static final String PASS = "YourPassword";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);  
