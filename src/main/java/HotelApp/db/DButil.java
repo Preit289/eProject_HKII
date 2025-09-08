b4cd8bbec38b0f4517c7e23a23a2a5d3c3e859be
LQHuy0210 <huylqts2206012@fpt.edu.vn>
2025-09-08 18:55:31 +0700

    fix: update DBUtil

--- src/main/java/HotelApp/db/DButil.java
+++ src/main/java/HotelApp/db/DButil.java
@@ -4,11 +4,13 @@ import java.sql.*;
 
 public class DButil {
     private static final String URL  =
-        "jdbc:sqlserver://localhost:1433;databaseName=HotelMng;encrypt=true;trustServerCertificate=true";
-    private static final String USER = "QHuy";
-    private static final String PASS = "123";
+        "jdbc:sqlserver://localhost:1433;databaseName=HotelMng;trustServerCertificate=true;integratedSecurity=true";
+    //private static final String USER = "QHuy";
+    //private static final String PASS = "123";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);  
+//        return DriverManager.getConnection(URL, USER, PASS);       
+        return DriverManager.getConnection(URL);  
+  
     }
 }
