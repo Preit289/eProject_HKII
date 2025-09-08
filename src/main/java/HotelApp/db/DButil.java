c35c1c65231274decce0bf33ccf35194e9e21bc6
Preit289 <hoanthinh.david@gmail.com>
2025-09-08 18:06:16 +0700

    nothing

--- src/main/java/HotelApp/db/DButil.java
+++ src/main/java/HotelApp/db/DButil.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class DButil {
     private static final String URL  =
         "jdbc:sqlserver://localhost:1433;databaseName=HotelMng;encrypt=true;trustServerCertificate=true";
-    private static final String USER = "QHuy";
-    private static final String PASS = "123";
+    private static final String USER = "sa";
+    private static final String PASS = "Preit289##$";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);  
