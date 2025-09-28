071af56707237d70be7ae1126f9e1714de1b1116
Your Name <phuchoang0167@gmail.com>
2025-09-28 20:22:15 +0700

    fix: check out database

--- src/main/java/HotelApp/repository/CheckinRepository.java
+++ src/main/java/HotelApp/repository/CheckinRepository.java
@@ -24,9 +24,11 @@ public class CheckinRepository {
                         STRING_AGG(rm.Room_num, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Rooms,
                         STRING_AGG(rm.Room_category, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Categories,
                         STRING_AGG(rm.Room_quality, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Types
-                    FROM Booking_Management bm
+                    FROM Staying_Management sm
+                    JOIN Booking_Management bm ON sm.Booking_id = bm.Booking_id
                     JOIN Booking_Room br ON bm.Booking_id = br.Booking_id
                     JOIN Room_Management rm ON br.Room_id = rm.Room_id
+                    WHERE sm.Staying_status = 0
                     GROUP BY bm.Book_by, bm.Book_contact, bm.Planned_checkin_date, bm.Planned_checkout_date
                 """;
 
@@ -71,12 +73,13 @@ public class CheckinRepository {
                         STRING_AGG(rm.Room_num, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Rooms,
                         STRING_AGG(rm.Room_category, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Categories,
                         STRING_AGG(rm.Room_quality, ', ') WITHIN GROUP (ORDER BY rm.Room_num) AS Types
-                    FROM Booking_Management bm
+                    FROM Staying_Management sm
+                    JOIN Booking_Management bm ON sm.Booking_id = bm.Booking_id
                     JOIN Booking_Room br ON bm.Booking_id = br.Booking_id
                     JOIN Room_Management rm ON br.Room_id = rm.Room_id
-                    WHERE bm.Book_contact LIKE ?
+                    WHERE sm.Staying_status = 0 AND bm.Book_contact LIKE ?
                     GROUP BY bm.Book_by, bm.Book_contact, bm.Planned_checkin_date, bm.Planned_checkout_date
-                """;
+                """ ;
 
         try (Connection conn = DButil.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
