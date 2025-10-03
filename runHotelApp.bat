3460b8d845b9c8f1e7ea0a58a60c9750fef7eec3
Harori <phamdangvinh2002@gmail.com>
2025-10-03 14:14:43 +0700

    docs: Installation Guide updated

--- runHotelApp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\hoant\Documents\NetBeansProjects\eProject_HKII\target"
-java --enable-native-access=javafx.graphics --module-path "C:\Users\hoant\Downloads\openjfx-24.0.2_windows-x64_bin-sdk (1)\javafx-sdk-24.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar HotelManagementSystemFXML-1.0-SNAPSHOT.jar
-pause
